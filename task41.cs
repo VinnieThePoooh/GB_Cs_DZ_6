9a43c0e41d7e5f681f5da118a6035cf6906a597c
Керим Гучигов <forsyte12316gmail.com@MacBook-Air-Kerim.local>
2023-03-08 08:50:30 +0300

    .

---
+++ task41.cs
@@ -0,0 +1,17 @@
+﻿// Считает сколько чисел больше 0 ввел пользователь
+
+System.Console.WriteLine("Введите числа через пробел");
+
+int[] array = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+void Sum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i< array.Length; i++)
+    {
+        if (array[i]>0) sum++;
+    }
+    System.Console.WriteLine("");
+    System.Console.WriteLine(sum);
+}
+
+Sum(array);
--- task43.cs
+++ task43.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения
+//  двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//  значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void Solve(double k1, double b1, double k2, double b2)
+{
+    // k1*x +b1=k2*x+b2;
+    //(k1*x)-(k2*x)=b2-b1;
+    //x(k1-k2)=(b2-b1);
+    //x = (b2-b1)/(k1-k2)
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1*x+b1;
+   System.Console.WriteLine(x+"," +y);
+}
+System.Console.WriteLine("Введите k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+Solve( k1,  b1,  k2,  b2);
